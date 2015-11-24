b0d93a461138d5a1086fbf235a3e848a4ce6b8ac
thrinay <thrinaygifts@yahoo.in>
2015-11-24 19:35:18 +0100

    intial commit

---
+++ login.js
@@ -0,0 +1,21 @@
+/**
+ * Created by thrinay on 11/24/2015.
+ */
+function submitFunction()
+{
+    var x=document.forms["myForm"]["username"].value;
+    var y=document.forms["myForm"]["password"].value;
+    if(x==null ||x=="" ||y==null ||y=="")
+    {
+        alert("Both fields cannot be emplty.");
+    }
+    else {
+        if (x=="admin" && y== "admin")
+        {
+            alert("You re logged in");
+                    }
+        else {
+            alert("Username incorrect");
+        }
+    }
+}
