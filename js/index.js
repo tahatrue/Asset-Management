31647a3f7119444406b23d5ea5028c16f9581504
thrinay <thrinaygifts@yahoo.in>
2015-11-27 19:05:19 +0100

    v1

---
+++ js/index.js
@@ -0,0 +1,18 @@
+function submitFunction()
+{
+	var x=document.forms["myForm"]["username"].value;
+	var y=document.forms["myForm"]["password"].value;
+	if(x==null ||x=="" ||y==null ||y=="")
+	{
+		alert("Both fields cannot be emplty.");
+	}
+	else {
+		if (x=="admin" && y== "admin")
+		{
+			alert("You re logged in");
+		}
+		else {
+			alert("Username or password is incorrect");
+		}
+	}
+}
